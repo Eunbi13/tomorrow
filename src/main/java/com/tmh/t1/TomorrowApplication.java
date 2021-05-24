83125e1a8eae1c1a4dcc48e913b6b40819a9f4a3
My study Storage <78071935+Eunbi13@users.noreply.github.com>
2021-05-24 14:31:33 +0900

    Initial commit

---
+++ src/main/java/com/tmh/t1/TomorrowApplication.java
@@ -0,0 +1,13 @@
+package com.tmh.t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomorrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomorrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmh/t1/TomorrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmh.t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
