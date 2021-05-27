d7aaa451ca30dda5e5ab9acb1d83537c30e04c94
kodahee <daheeko0604@naver.com>
2021-05-27 17:30:33 +0900

    -

--- src/main/java/com/tmh/t1/housewarming/HousewarmingController.java
+++ src/main/java/com/tmh/t1/housewarming/HousewarmingController.java
@@ -1,7 +1,10 @@
 package com.tmh.t1.housewarming;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,8 +16,9 @@ public class HousewarmingController {
 	HousewarmingService housewarmingService;
 	
 	@GetMapping("list")
-	public void getList() throws Exception {
-		
+	public void getList(Model model) throws Exception {
+		List<HousewarmingVO> ar = housewarmingService.getList();
+		model.addAttribute("list", ar);
 	}
 
 }
