801f2f6bdbe67d101225b0814795948315a74379
ba-lim <dmsqltkfk276@naver.com>
2021-05-30 00:55:28 +0900

    Update BrandVO.java

--- src/main/java/com/tmh/t1/brand/BrandVO.java
+++ src/main/java/com/tmh/t1/brand/BrandVO.java
@@ -27,9 +27,16 @@ public class BrandVO {
 	private String brandIntro;
 	private String referenceFile;
 	private String oriFile;//DB에는 안담고 오리지널 이름 표기용으로 사용 
-	private boolean accept;//false여야 함
+	private boolean accept;
 	
 	//category 가져와야함 brand_category를 거쳐서 쿼리문 짜기 
 	//임시 파일 이 파일 지울때 config도 수정해야함 
 	private List<CategoryVO> categories;
+	
+	public boolean isAccept() {
+		this.accept=false;
+		return this.accept;
+	}
+	
+	
 }
