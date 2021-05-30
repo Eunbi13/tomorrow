ebc9edc5a0f5eb6f356c9863265dd9306d555564
Euuu127 <mak12cht7@gmail.com>
2021-05-31 07:34:53 +0900

    cat

---
+++ src/main/java/com/tmh/t1/category/CategoryMapper.java
@@ -0,0 +1,9 @@
+package com.tmh.t1.category;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CategoryMapper {
+
+	public CategoryVO selectCategory(CategoryVO categoryVO)throws Exception;
+}
