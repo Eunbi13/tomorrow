346cd5e06f86d79262574c38291f3b5fcdaf7452
Euuu127 <mak12cht7@gmail.com>
2021-06-24 14:37:12 +0900

    op

--- src/main/java/com/tmh/t1/product/ProductVO.java
+++ src/main/java/com/tmh/t1/product/ProductVO.java
@@ -24,7 +24,7 @@ public class ProductVO {
 
 	private ProductImagesVO productImagesVO;
 	private List<ProductImagesVO> images;
-//	private List<OptionsVO> optionsVOs;
+	private List<OptionsVO> optionsVOs;
 //	private CategoryVO categoryVO;
 	
 	private BrandVO brandVO;
